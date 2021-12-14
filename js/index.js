22bb28fc059f3e3165d697fb494c731d462df91a
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2021-12-14 11:14:23 +0600

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+//document.write("Access Denied!");
+//if the day is saturday, then we allowed
+//else block
+
+const date=new Date();
+if(date.getDate()==20)
+{
+    alert("allow to visit");
+}
+else
+{
+    document.write("Access Denied!");
+}
